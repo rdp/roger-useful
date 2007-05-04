11224756fdb43dc0420080a8a0f7dc1eb87eabbf
Roger Pack <rogerpack2005@gmail.com>
2007-05-04 16:12:32 +0000

    arra

--- useful_ruby_utilities.rb
+++ useful_ruby_utilities.rb
@@ -114,10 +114,21 @@ def greatestValue
                    end
                     end
                      return greatestValue
-                     end
-                     end
+                   end
+                   
+  def ifOrderedSumOfValuesUpToAndIncludingKey(maxKey)
+    sum = 0
+    # self.sort is an array of pairs [[key, value], [key, value]...]
+    for key, value in self.sort do 
+      if key > maxKey
+          break
+        end
+      sum += value
+    end
+    return sum
+  end
+end
                      
-
 
 class TCPsocket
 
@@ -153,3 +164,85 @@ end
 #   end
 # }
 
+
+class Array # from http://snippets.dzone.com/posts/show/898
+  # Chooses a random array element from the receiver based on the weights
+  # provided. If _weights_ is nil, then each element is weighed equally.
+  # 
+  #   [1,2,3].random          #=> 2
+  #   [1,2,3].random          #=> 1
+  #   [1,2,3].random          #=> 3
+  #
+  # If _weights_ is an array, then each element of the receiver gets its
+  # weight from the corresponding element of _weights_. Notice that it
+  # favors the element with the highest weight.
+  #
+  #   [1,2,3].random([1,4,1]) #=> 2
+  #   [1,2,3].random([1,4,1]) #=> 1
+  #   [1,2,3].random([1,4,1]) #=> 2
+  #   [1,2,3].random([1,4,1]) #=> 2
+  #   [1,2,3].random([1,4,1]) #=> 3
+  #
+  # If _weights_ is a symbol, the weight array is constructed by calling
+  # the appropriate method on each array element in turn. Notice that
+  # it favors the longer word when using :length.
+  #
+  #   ['dog', 'cat', 'hippopotamus'].random(:length) #=> "hippopotamus"
+  #   ['dog', 'cat', 'hippopotamus'].random(:length) #=> "dog"
+  #   ['dog', 'cat', 'hippopotamus'].random(:length) #=> "hippopotamus"
+  #   ['dog', 'cat', 'hippopotamus'].random(:length) #=> "hippopotamus"
+  #   ['dog', 'cat', 'hippopotamus'].random(:length) #=> "cat"
+  def randomItem(weights=nil)
+    return random(map {|n| n.send(weights)}) if weights.is_a? Symbol
+    
+    weights ||= Array.new(length, 1.0)
+    total = weights.inject(0.0) {|t,w| t+w}
+    point = rand * total
+    
+    zip(weights).each do |n,w|
+      return n if w >= point
+      point -= w
+    end
+  end
+  
+  # Generates a permutation of the receiver based on _weights_ as in
+  # Array#random. Notice that it favors the element with the highest
+  # weight.
+  #
+  #   [1,2,3].randomize           #=> [2,1,3]
+  #   [1,2,3].randomize           #=> [1,3,2]
+  #   [1,2,3].randomize([1,4,1])  #=> [2,1,3]
+  #   [1,2,3].randomize([1,4,1])  #=> [2,3,1]
+  #   [1,2,3].randomize([1,4,1])  #=> [1,2,3]
+  #   [1,2,3].randomize([1,4,1])  #=> [2,3,1]
+  #   [1,2,3].randomize([1,4,1])  #=> [3,2,1]
+  #   [1,2,3].randomize([1,4,1])  #=> [2,1,3]
+  def randomizedCopy(weights=nil)
+    return randomize(map {|n| n.send(weights)}) if weights.is_a? Symbol
+    
+    weights = weights.nil? ? Array.new(length, 1.0) : weights.dup
+    
+    # pick out elements until there are none left
+    list, result = self.dup, []
+    until list.empty?
+      # pick an element
+      result << list.random(weights)
+      # remove the element from the temporary list and its weight
+      weights.delete_at(list.index(result.last))
+      list.delete result.last
+    end
+    
+    result
+  end
+  
+  def eachInRandomOrderWithIndex
+   arrayOfNumbersRepresentingUnchosenLocations = (0..(self.length - 1)).to_a
+   while arrayOfNumbersRepresentingUnchosenLocations.length > 0
+     indexIntoArrayOfUnchosenNumbers = rand(arrayOfNumbersRepresentingUnchosenLocations.length) # rand  index 
+     newRandomNumber = arrayOfNumbersRepresentingUnchosenLocations.slice!(indexIntoArrayOfUnchosenNumbers, 1)[0] # slice it out
+     yield self[newRandomNumber], newRandomNumber
+   end
+  end
+  
+  
+end
