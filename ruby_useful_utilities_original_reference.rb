81172f05ef3e22d0e6125b978422745fe2b14a21
Roger Pack <rogerpack2005@gmail.com>
2007-06-25 06:54:25 +0000

    syntax, I...think...

--- ruby_useful_utilities_original_reference.rb
+++ ruby_useful_utilities_original_reference.rb
@@ -8,99 +8,99 @@ class AssertionFailure < StandardError # from http://blade.nagaokaut.ac.jp/cgi-b
 end
 
 def goGetFile(urlToGet, whereItGoes = nil)
-    wgetCommand = "wget #{urlToGet}"
-    if whereItGoes
-      wgetCommand += " -O #{whereItGoes}"
-    end
-   # todo double check this, see if wget exists, etc...
-    if system(wgetCommand)
-      return
-    else
-      require 'open-uri'
-      writeOut = open(whereItGoes, "wb")
-      writeOut.write(open(urlToGet).read)
-      writeOut.close
-      print "ACK NO WGET!"
-      #puts "downloaded" + urlToGet + "\n"
-    end
-    
+  wgetCommand = "wget #{urlToGet}"
+  if whereItGoes
+    wgetCommand += " -O #{whereItGoes}"
+  end
+  # lodo double check this, see if wget exists, etc...
+  if system(wgetCommand)
+    return
+  else
+    require 'open-uri'
+    writeOut = open(whereItGoes, "wb")
+    writeOut.write(open(urlToGet).read)
+    writeOut.close
+    print "ACK NO WGET!"
+    #puts "downloaded" + urlToGet + "\n"
+  end
+  
 end
 
 def isGoodExecutableFile?(thisFileName)
   if File.executable_real? thisFileName
     return true
   end
- begin
+  begin
     a = IO.popen(thisFileName, "w+")
     returnVal = true
     # if that cleared then the exec worked
     a.close
- rescue => details
-  returnVal = false
- end
- return returnVal # lodo there's some bug with this
+  rescue => details
+    returnVal = false
+  end
+  return returnVal # lodo there's some bug with this
 end
 
 
 class String
-
-def sanitize
-  self.gsub(/[`\.?*\/\\|<>!&;:"'~@#\$%\^\(\)]/, '_')
-end
-def sanitize!
-  self.gsub!(/[`\.?*\/\\|<>!&;:"'~@#\$%\^\(\)]/, '_')
-end
-
-def shiftOutputAndResulting number
-  if number.class == Float
-    number = number.ceil
- #   print "warning you passed me a float? huh?\n" lodo debug
+  
+  def sanitize
+    self.gsub(/[`\.?*\/\\|<>!&;:"'~@#\$%\^\(\)]/, '_')
   end
-  assert number != 0
-  output = self[0..(number -1)]
-  rest = self[number..10000000]
-  if rest != nil and rest.length == 0
-    rest = nil
+  def sanitize!
+    self.gsub!(/[`\.?*\/\\|<>!&;:"'~@#\$%\^\(\)]/, '_')
   end
-  return output, rest # lodo better funcs
-
-end
+  
+  def shiftOutputAndResulting number
+    if number.class == Float
+      number = number.ceil
+      #   print "warning you passed me a float? huh?\n" lodo debug
+    end
+    assert number != 0
+    output = self[0..(number -1)]
+    rest = self[number..10000000]
+    if rest != nil and rest.length == 0
+      rest = nil
+    end
+    return output, rest # lodo better funcs
+    
+  end
 end
 
 
 class Object
   def assert(bool, message = 'assertion failure')
-      # this means 'if the -d command passed to ruby-- we don't use that every right now...if $DEBUG
-                if not bool
-                  print "ack assertion failed! [#{message}]\n"
-                  begin
-                    raise AssertionFailure.new(message)
-                  rescue AssertionFailure => a
-                    print a.backtrace.join("\n")
-                    print "\n"
-                    raise
-                  end
-               end
+    # this means 'if the -d command passed to ruby-- we don't use that every right now...if $DEBUG
+    if not bool
+      print "ack assertion failed! [#{message}]\n"
+      begin
+        raise AssertionFailure.new(message)
+      rescue AssertionFailure => a
+        print a.backtrace.join("\n")
+        print "\n"
+        raise
+      end
+    end
   end
 end
 
 def assertEqual(a, b, errorString = "")
-        if a != b
-            message = "ERROR NOT EQUAL: [" + a.to_s + "] != [" + b.to_s + "]\n"
-            print message
-            pp a, "!=", b
-            raise AssertionFailure.new(message)
-        end
+  if a != b
+    message = "ERROR NOT EQUAL: [" + a.to_s + "] != [" + b.to_s + "]\n"
+    print message
+    pp a, "!=", b
+    raise AssertionFailure.new(message)
+  end
 end
 
 class Dir
-class << self
-
-  def mkPath path
-  # lodo File.directory? "good_dir/bad_dir" -> True
-    oldDir = Dir.pwd
-    finalDir = "/"
-    for part in path.split("/")
+  class << self
+    
+    def mkPath path
+      # lodo File.directory? "good_dir/bad_dir" -> True
+      oldDir = Dir.pwd
+      finalDir = "/"
+      for part in path.split("/")
         if part != ""
           finalDir = finalDir + part + "/"
           if not File.directory? part then 
@@ -108,61 +108,61 @@ class << self
           end
           Dir.chdir(part) # unfortunately necessary
         end
+      end
+      Dir.chdir(oldDir)
     end
-    Dir.chdir(oldDir)
+    
   end
-  
-end
 end
 
 require 'socket' # gotta override a previously instantiated socket! eck
 class Socket
-        class << self
-          def gethostip
-                  return getHostIP
-          end
-          def getHostIP
-              ipInt = gethostbyname(gethostname())[3]
-              return "%d.%d.%d.%d" % [ipInt[0], ipInt[1], ipInt[2], ipInt[3]]
-          end
-          
-        end
+  class << self
+    def gethostip
+      return getHostIP
+    end
+    def getHostIP
+      ipInt = gethostbyname(gethostname())[3]
+      return "%d.%d.%d.%d" % [ipInt[0], ipInt[1], ipInt[2], ipInt[3]]
+    end
+    
+  end
 end
 
 
 class TCPSocket
-          attr_accessor :amPastHeader 
-
-          def nukeHeaderAndSetBoolIfThere(fromThis)
-              locationOfSplit = fromThis.index("\r\n\r\n")
-              if locationOfSplit != nil
-                    self.amPastHeader = true
-                    returnable = fromThis[locationOfSplit + 4..1000000] # what a hack!
-                    print "returning #{returnable}"
-                    return returnable
-              else
-                    print "very odd! -- [#{fromThis}] must be in the header still!"
-                    return ""
-                    
-               end
-          end
+  attr_accessor :amPastHeader 
+  
+  def nukeHeaderAndSetBoolIfThere(fromThis)
+    locationOfSplit = fromThis.index("\r\n\r\n")
+    if locationOfSplit != nil
+      self.amPastHeader = true
+      returnable = fromThis[locationOfSplit + 4..1000000] # what a hack!
+      print "returning #{returnable}"
+      return returnable
+    else
+      print "very odd! -- [#{fromThis}] must be in the header still!"
+      return ""
+      
+    end
+  end
 end
 
 class Object
-
- def methodLookup(methodName)
+  
+  def methodLookup(methodName)
     classname = self.class
     
     if classname == "Class" # then methodLookup is being called on a class itself, ala "String.methodLookup(x)" instead of "stringInstance.methodLookup(x)"
-        classname = self.name
-   end
-   result  = system("ri.bat \"%s.%s\" --no-pager" % [classname, methodName]) # only works in windows, here
-   if not result
-       result  = system("ri \"%s.%s\" --no-pager" % [classname, methodName]) # may work better in linux
-   end
-   return result
-   end
-
+      classname = self.name
+    end
+    result  = system("ri.bat \"%s.%s\" --no-pager" % [classname, methodName]) # only works in windows, here
+    if not result
+      result  = system("ri \"%s.%s\" --no-pager" % [classname, methodName]) # may work better in linux
+    end
+    return result
+  end
+  
 end # Object
 
 def writeToFile(a)
@@ -174,24 +174,24 @@ def readFromFile(b)
 end
 
 class Fixnum
- def asIfChar
-  return "%c" % self
- end
+  def asIfChar
+    return "%c" % self
+  end
 end
 
 # and its opposite--kind of -- a very broken string to ascii
 class String
- def firstCharToAscii
-        return ("%d" % self[0]).to_i
- end
-
- def firstCharToBinary
-        return  "%b" % firstCharToAscii()
- end
+  def firstCharToAscii
+    return ("%d" % self[0]).to_i
+  end
+  
+  def firstCharToBinary
+    return  "%b" % firstCharToAscii()
+  end
 end
 
 class Hash
-
+  
   def divideValuesBy(this)
     this = this.to_f
     output = {}
@@ -199,9 +199,9 @@ class Hash
       output[key] = self[key] / this
     }
     return output
-
+    
   end
-
+  
   def multiplyKeysBy(this)
     output = {}
     self.each_key { |key|
@@ -214,25 +214,25 @@ class Hash
     output = {}
     self.each_key { |key|
       output[key.to_i] = self[key]
-    
+      
     }
     return output
+    
+  end
   
-  end
-
   def sumValues
-   sum = nil
-   self.each_key { |key|
-    if not sum.nil?
-      sum += self[key]
-    else
-      sum = self[key] # keep this non class specific :)
-    end
-   
-   }
-   return sum
+    sum = nil
+    self.each_key { |key|
+      if not sum.nil?
+        sum += self[key]
+      else
+        sum = self[key] # keep this non class specific :)
+      end
+      
+    }
+    return sum
   end
-
+  
   def addToKey(key, addThis)
     if self.has_key? key
       self[key] += addThis
@@ -248,58 +248,63 @@ class Hash
       return 0
     end
   end
-
-
- def pairWithGreatestKey # same as max but doesn't err... todo report!
- greatestKeyPair = nil
- self.each_pair do |key, value|
-     if greatestKeyPair == nil or key > greatestKeyPair[0] # todo more fast!
-       greatestKeyPair = [key, value] # to be able to pass them both out
-     end
+  
+  
+  def pairWithGreatestKey # same as max but doesn't err... lodo report to ruby!
+    greatestKeyPair = nil
+    self.each_pair do |key, value|
+      if not greatestKeyPair or key > greatestKeyPair[0] # lodo more fast! submit to ruby!
+        greatestKeyPair = [key, value] # to be able to pass them both out
+      end
+    end
+    return greatestKeyPair
   end
- return greatestKeyPair
- end
-
-def greatestValue
-
- greatestValue = nil
-  self.each_pair do |key, value|
-     if greatestValue == nil or value > greatestValue[1] # todo more fast!
-                greatestValue = [key, value]
-                   end
-                    end
-                     return greatestValue
-                   end
-                   
+  
+  def greatestValue
+    
+    greatestValue = nil
+    self.each_pair do |key, value|
+      if greatestValue == nil or value > greatestValue[1] # lodo more fast (?)!
+        greatestValue = [key, value]
+      end
+    end
+    return greatestValue
+  end
+  
   def ifOrderedSumOfValuesUpToAndIncludingKey(maxKey)
     sum = 0
     # self.sort is an array of pairs [[key, value], [key, value]...]
     for key, value in self.sort do 
       if key > maxKey
-          break
-        end
+        break
+      end
       sum += value
     end
     return sum
   end
 end
-                     
+
 
 class TCPsocket
-
-def writeReliable(stuffIn)
-  totalToSend = stuffIn.length
-  totalSent = 0
-  while totalSent < totalToSend do
-      if totalSent > 0
-          print "writeReliable looped!!!!i once you see this once then mark it as useful, comment out"
-      end
-      received = write(stuffIn)
-      stuffIn = stuffIn[received..10000000] # todo find a better way :)
-      totalSent += received
-  end# todo test!
-  flush
-end
+  
+  def writeReliable(stuffIn)
+    amountWrote = write(stuffIn)
+    assert(amountWrote == stuffIn.length, "ack a socket right (roger) failed! fix!")
+    flush # I have no idea if this does anything
+    # rest seems unnecessary
+    #  totalToSend = stuffIn.length
+    #  totalSent = 0
+    #  while totalSent < totalToSend do
+    #      if totalSent > 0
+    #          print "writeReliable looped!!!!i once you see this once then mark it as useful, comment out"
+    #      end
+    #      received = write(stuffIn)
+    #      stuffIn = stuffIn[received..10000000] # lodo find a better way :)
+    #      totalSent += received
+    #  end# lodo test -- appears unnecessary!
+    #  flush
+    return amountWrote
+  end
 end
 
 # code for exception handling
@@ -325,7 +330,7 @@ end
 
 
 class Float
-
+  
   def truncateToDecimal(decimal)
     return ("%.0#{decimal}f" % self).to_f
   end
@@ -338,53 +343,53 @@ class Array
       if thread.alive?
         out << thread
       else
-       # it is dead, let it die
+        # it is dead, let it die
       end
     end
     return out
     
   end
-
-    def joinOnAllThreadsInArray
-      while not self.empty?
-       waitForThisThread = self.shift
-       assert waitForThisThread.class == Thread
-       waitForThisThread.join
-      end
-    end
-    
-    def collapsePointsToIntegers
-      return toSummedByIntegerHash.sort
-    end
-    
-   def toSummedByIntegerHash # lodo rename sum
-      finalArray = {}
-      self.each { |pointDuple|
-        finalArray.addToKey(pointDuple[0].to_i, pointDuple[1])
-      }
-      return finalArray
-   end
-   
-   def dupleArrayToSummedHash
-      finalArray = {}
-      self.each { |pointDuple|
-        finalArray.addToKey(pointDuple[0], pointDuple[1])
-      }
-      return finalArray
-    
-   end
-   
-def average
-  sum = 0
-  self.each { |entry|
-   sum += entry
-  }
-  return sum / self.length
-end
   
-
-
- # from http://snippets.dzone.com/posts/show/898
+  def joinOnAllThreadsInArray
+    while not self.empty?
+      waitForThisThread = self.shift
+      assert waitForThisThread.class == Thread
+      waitForThisThread.join
+    end
+  end
+  
+  def collapsePointsToIntegers
+    return toSummedByIntegerHash.sort
+  end
+  
+  def toSummedByIntegerHash # lodo rename sum
+    finalArray = {}
+    self.each { |pointDuple|
+      finalArray.addToKey(pointDuple[0].to_i, pointDuple[1])
+    }
+    return finalArray
+  end
+  
+  def dupleArrayToSummedHash
+    finalArray = {}
+    self.each { |pointDuple|
+      finalArray.addToKey(pointDuple[0], pointDuple[1])
+    }
+    return finalArray
+    
+  end
+  
+  def average
+    sum = 0
+    self.each { |entry|
+      sum += entry
+    }
+    return sum / self.length
+  end
+  
+  
+  
+  # from http://snippets.dzone.com/posts/show/898
   # Chooses a random array element from the receiver based on the weights
   # provided. If _weights_ is nil, then each element is weighed equally.
   # 
@@ -455,12 +460,12 @@ end
   end
   
   def eachInRandomOrderWithIndex
-   arrayOfNumbersRepresentingUnchosenLocations = (0..(self.length - 1)).to_a
-   while arrayOfNumbersRepresentingUnchosenLocations.length > 0
-     indexIntoArrayOfUnchosenNumbers = rand(arrayOfNumbersRepresentingUnchosenLocations.length) # rand  index 
-     newRandomNumber = arrayOfNumbersRepresentingUnchosenLocations.slice!(indexIntoArrayOfUnchosenNumbers, 1)[0] # slice it out
-     yield self[newRandomNumber], newRandomNumber
-   end
+    arrayOfNumbersRepresentingUnchosenLocations = (0..(self.length - 1)).to_a
+    while arrayOfNumbersRepresentingUnchosenLocations.length > 0
+      indexIntoArrayOfUnchosenNumbers = rand(arrayOfNumbersRepresentingUnchosenLocations.length) # rand  index 
+      newRandomNumber = arrayOfNumbersRepresentingUnchosenLocations.slice!(indexIntoArrayOfUnchosenNumbers, 1)[0] # slice it out
+      yield self[newRandomNumber], newRandomNumber
+    end
   end
   
   
