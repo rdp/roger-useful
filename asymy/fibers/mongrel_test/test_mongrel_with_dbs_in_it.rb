5ff7768ed6c5fde2df74658de3899e967df3d483
Roger Pack <rogerpack2005@gmail.com>
2008-06-13 23:08:27 +0000

    working mongrel example

--- asymy/fibers/mongrel_test/test_mongrel_with_dbs_in_it.rb
+++ asymy/fibers/mongrel_test/test_mongrel_with_dbs_in_it.rb
@@ -1,12 +1,12 @@
 require 'rubygems'
 require 'swiftcore/evented_mongrel'
-#require 'mongrel'
 require '../../source/asymy/asymy'
 require '../setup_db_opts'
 
  class SimpleHandler < Mongrel::HttpHandler
     @@count_ever = 0
     def process(request, response)
+      # note that @'s here are shared among mongrel's incoming connections!--don't use them
       my_count = @@count_ever
       @@count_ever += 1
       response.start(200) do |head,out|
@@ -14,15 +14,16 @@ require '../setup_db_opts'
         head["Content-Type"] = "text/plain"
         out.write("hello!\n")
 
-	@conn = Asymy::Connection.new $opts
+	conn = Asymy::Connection.new $opts
 	5.times { |n|
 	   local_var = nil # an example of how to set local vars :)
 	   size = [:big, :small][my_count % 2]
 	   query = "select count(*) from #{ size == :small ? 'users' : 'user_sessions' }"
 	   print "starting #{my_count} #{size} #{n}\n"
-           a = @conn.exec_and_fiber_yield(query) {|h, c| local_var = [h,c]} # a == local_var
+           a = conn.exec_and_fiber_yield(query) {|h, c| local_var = [h,c]} # a == local_var
            print "done #{my_count} #{size} #{n}\n"
 	}
+	conn.em_connection.close_connection
 
       end
     end
